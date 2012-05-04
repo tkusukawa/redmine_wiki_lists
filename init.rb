72151b816631d589d5609616865b7462be9b4a9c
kusu <devnull@localhost>
2012-05-05 05:06:36 +0900

    create new redmine plugin

---
+++ init.rb
@@ -0,0 +1,14 @@
+require 'redmine'
+
+Dir::foreach(File.join(File.dirname(__FILE__), 'lib')) do |file|
+  next unless /\.rb$/ =~ file
+  require file
+end
+
+Redmine::Plugin.register :redmine_wiki_lists do
+  name 'Redmine Wiki Lists plugin'
+  author 'Tomohisa Kusukawa'
+  description 'wiki macros to display lists of contents.'
+  version '0.0.0'
+  url 'https://bitbucket.org/kusu/redmine_wiki_lists'
+end
