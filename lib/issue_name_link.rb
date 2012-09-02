b7b215cd58af8522f647f792a8065fe45f29023e
kusu <devnull@localhost>
2012-09-03 00:00:58 +0900

    fixes #1105 チケットのタイトルを指定してチケットへのリンクが作れるマクロが欲しい

---
+++ lib/issue_name_link.rb
@@ -0,0 +1,39 @@
+require 'redmine'
+
+module WikiListsRefIssue
+  Redmine::WikiFormatting::Macros.register do
+    desc "make a link of a issue by its subject."
+    macro :issue_name_link do |obj, args|
+      out=""
+      begin
+        raise "no parameters" if args.count==0
+        raise "too many parameters" if args.count>1
+        arg=args.shift
+        arg.strip!
+        if arg=~/^([^:]*):([^:]*)$/ then
+          prj=Project.find_by_name($1)
+          raise "project:#{$1} is not found." unless prj
+          arg=$2
+        else
+          prj=obj.project
+        end
+        if arg=~/^([^\|]*)\|([^\|]*)$/ then
+          name=$1
+          disp=$2
+        else
+          name=arg
+          disp=arg
+        end
+        cond="project_id=#{prj.id} AND subject='#{name}'"
+        issue = Issue.find(:first, :conditions=>cond)
+        raise "issue:#{name} is not found in prj:#{prj.to_s}" unless issue
+        Issue.find_by_subject(name)
+        out << link_to("#{disp}", {:controller => "issues", :action => "show", :id => issue.id})
+      rescue => err_msg
+        raise "parameter error: #{err_msg}<br>"+
+          "usage: {{issue_name_link([project_name:]issue_subject[|description])}}"
+      end
+      out
+    end
+  end
+end
