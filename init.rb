0238aa535e8efb441a551251d54815780d9fb3ef
kusu <devnull@localhost>
2012-09-09 16:34:25 +0900

    Version 0.0.1

--- init.rb
+++ init.rb
@@ -9,6 +9,6 @@ Redmine::Plugin.register :redmine_wiki_lists do
   name 'Redmine Wiki Lists plugin'
   author 'Tomohisa Kusukawa'
   description 'wiki macros to display lists of contents.'
-  version '0.0.0'
+  version '0.0.1'
   url 'https://bitbucket.org/kusu/redmine_wiki_lists'
 end
