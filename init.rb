7d5c3c096b892f4d0f409a370e73578e3394bc6a
Tomohisa Kusukawa <t.kusukawa@gmail.com>
2015-07-25 17:32:38 +0900

    Version 0.0.6

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_wiki_lists do
   name 'Redmine Wiki Lists plugin'
   author 'Tomohisa Kusukawa'
   description 'wiki macros to display lists of issues.'
-  version '0.0.5'
+  version '0.0.6'
   url 'http://www.r-labs.org/projects/wiki_lists/wiki/Wiki_Lists'
   author_url 'http://bitbucket.org/tkusukawa'
 end
